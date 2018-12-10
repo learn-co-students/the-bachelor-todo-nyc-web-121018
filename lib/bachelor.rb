19571054831cfd5a8dc86a2c94a608d4328980ce
Daniel Fries <daniel.w.fries@gmail.com>
2018-12-10 02:04:30 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,19 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
   # code here
+  name = ""
+  data[season].each do |contestant_hash|
+##    contestant_hash.each do |stat_key, stat_val|
+##      binding.pry
+      if contestant_hash["status"].include?("Winner")
+        name.concat(contestant_hash["name"])
+##      binding.pry
+##      end
+      end
+end
+name.split(" ")
+name(1)
 end
 
 def get_contestant_name(data, occupation)
